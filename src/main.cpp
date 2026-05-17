db1ab02687e802a594f689d28b9a467313c690e7
longlonger2022 <lgr_longer@outlook.com>
2026-05-17 13:42:01 +0800

    docs(main): 添加 main.cpp 文件头注释

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,14 @@
+/*
+ * Universal-Timer
+ * Copyright (c) 2025-2026 龙ger_longer
+ * Author: 龙ger_longer
+ * Developer: 龙ger_longer, new_pointer
+ * Version: 2.1.0
+ * License: GNU General Public License v3.0
+ * Repository: https://github.com/longlonger2022/Universal-Timer
+ * Description: A useful countdown timer application.
+*/
+
 #include "core/UniversalTimer2.h"
 #include "core/LogManager.h"
 
